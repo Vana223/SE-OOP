9298478da4bb06b407772e280a76583a0f8840fc
Ivan Sonyk <104148733+Vana223@users.noreply.github.com>
2024-04-01 18:40:53 +0000

    task 1.1

---
+++ src/Task 16/src/task.java
@@ -0,0 +1,16 @@
+public class task {
+    public static void main(String[] args) {
+        double v0 = 10;
+        double alpha = Math.toRadians(45);
+        double g = 9.8;
+        double t;
+
+        for (int i = 0; i < 16; i++) {
+            t = i;
+            double x = v0 * Math.cos(alpha) * t;
+            double y = v0 * Math.sin(alpha) * t - (g * t * t) / 2;
+
+            System.out.println("Час " + t + ": x = " + x + ", y = " + y);
+        }
+    }
+}
